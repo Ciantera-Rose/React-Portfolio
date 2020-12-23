f12379b76bec0c0e5ae71dd0c52d4f58c9460883
Ciantera-Rose <ciantera.rose@gmail.com>
2020-12-23 17:46:15 -0500

    ternary conditional button

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
 
 import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container"
 
 export default class App extends Component {
   render() {
     return ( 
       <div className='app'>
+        <NavigationContainer />
         <h1>Ciantera Rose React Portfolio</h1>
         <div>{moment().format("MMMM do YYYY, hh:mm:ss a")}</div>
         <PortfolioContainer />
