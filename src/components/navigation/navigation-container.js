8c5b57115820c249caf470e9d564e0395de0830d
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-04 17:15:02 -0500

    add nav-links

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,24 +1,29 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class NavigationContainer extends Component {
-    constructor() {
-      super()
-    }
-    adminLinks() {
-
-    }
-
-    render() {
-      return (
-        <div>
-          <button>Home</button>
-          <button>About</button>
-          <button>Contact</button>
-          <button>Blog</button>
-          <button>Add Blog</button>
-          {false ? <button>AddBlog</button> : null}
-        </div>
-      )
-    }
+  constructor() {
+    super();
   }
- 
+  adminLinks() {}
+
+  render() {
+    return (
+      <div>
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+
+        <NavLink exact to="/about-me">
+          About
+        </NavLink>
+
+        
+        <button>Contact</button>
+        <button>Blog</button>
+        <button>Add Blog</button>
+        {false ? <button>AddBlog</button> : null}
+      </div>
+    );
+  }
+}
