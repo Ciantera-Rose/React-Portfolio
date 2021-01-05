c0a65270da7944b00b935ef5e0ae10048fb11ec9
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-04 22:12:14 -0500

    add key prop for item id

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -43,7 +43,12 @@ export default class PortfolioContainer extends Component {
     return this.state.data.map((item) => {
       console.log("item data", item);
       return (
-        <PortfolioItem title={item.name} url={item.url} slug={item.id} />
+        <PortfolioItem
+          key={item.id}
+          title={item.name}
+          url={item.url}
+          slug={item.id}
+        />
       );
     });
   }
