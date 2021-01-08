9324969616eaa2f2c8ca6787284d35a7e9da7a3b
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-07 19:04:08 -0500

    add form, login and password handle events

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,14 +1,52 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+
+    handleChange = (e) => {
+        //console.log("handle change", e)
+        this.setState({
+            [e.target.name]: e.target.value
+
+        })
+    }
+
+    handleSubmit = (e) =>  {
+        console.log("handle submit", e)
+    }
+
     render() {
         return (
           <div>
             <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+           
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="email"
+                name="email"
+                placeholder="Your email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
 
-            <form>
-              <input type="text" />
-              <input type="password" />
+              <input
+                type="password"
+                name="password"
+                placeholder="Your password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+
+              <div>
+                <button type="submit">Login</button>
+              </div>
             </form>
           </div>
         );
