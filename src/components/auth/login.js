d8b372a851e88e0f79e10f6ecc26fbee8c0668a6
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-07 23:36:17 -0500

    handle sign in error

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -14,33 +14,48 @@ export default class Login extends Component {
     handleChange = (e) => {
         //console.log("handle change", e)
         this.setState({
-            [e.target.name]: e.target.value
-
+            [e.target.name]: e.target.value,
+            errorText: ""
         })
     }
 
     handleSubmit = (e) =>  {
         //console.log("handle submit", this.state.email, this.state.password)
-        axios.post("https://api.devcamp.space/sessions",
-        {
+        axios
+            .post(
+                "https://api.devcamp.space/sessions",
+            {
             client: {
                 email: this.state.email,
-                password: this.state.password
-
+                password: this.state.password,
+                errorText: ""
             }
         },
           { withCredentials: true }
-        ).then(response => {
-            console.log("response", response)
+        )
+        .then(response => {
+            if (response.data.status === "created") {
+                console.log("You can come in...")
+            } else {
+                this.setState({
+                    errorText: "Wrong email or password"
+                })
+            }
+        })
+        .catch(error => {
+            //console.log("error occured", error)
+            this.setState({
+                errorText: "An error occurred"
+            })
         })
         e.preventDefault()
-
     }
 
     render() {
         return (
           <div>
             <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+            <div>{this.state.errorText}</div>
            
             <form onSubmit={this.handleSubmit}>
               <input
