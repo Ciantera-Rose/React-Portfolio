5468ec1ef5ebe925ca28570278481a8eda878dca
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-11 18:34:49 -0500

    install and configure font awesome library

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -25,14 +25,12 @@ export default class PortfolioManager extends Component {
         `https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`,
         { withCredentials: true }
       )
-      .then((response) => {
+      .then(() => {
         this.setState({
           portfolioItems: this.state.portfolioItems.filter((item) => {
             return item.id !== portfolioItem.id;
           }),
         });
-
-        return response.data;
       })
       .catch((error) => {
         console.log("handleDeleteClick error", error);
