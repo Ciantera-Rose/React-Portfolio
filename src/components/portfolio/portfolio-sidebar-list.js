6c8a6428c71ffcf386e92b4c896d7a12c90662f4
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-11 19:31:26 -0500

    debug fa-trash icon errors, add sign out icon and styles

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -12,21 +12,12 @@ const PortfolioSidebarList = (props) => {
         <div className="text-content">
           <div className="title">{portfolioItem.name}</div>
 
-          <div className="actions">
-            <a
-              className="action-icon"
-              onClick={() => props.handleEditClick(portfolioItem)}
-            >
-              <FontAwesomeIcon icon="edit" />
-            </a>
-
-            <a
-              className="action-icon"
-              onClick={() => props.handleDeleteClick(portfolioItem)}
-            >
-              <FontAwesomeIcon icon="trash" />
-            </a>
-          </div>
+          <a
+            className="delete-icon"
+            onClick={() => props.handleDeleteClick(portfolioItem)}
+          >
+            <FontAwesomeIcon icon="trash" />
+          </a>
         </div>
       </div>
     );
