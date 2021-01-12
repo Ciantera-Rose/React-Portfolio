90b9020143a4bbdd261188fe00b87482decb0b7a
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-12 18:22:34 -0500

    add call blog API and store data in state

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 class Blog extends Component {
   constructor() {
     super();
+
+    this.state = {
+      blogItems: [],
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://cianterarose.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs,
+        });
+      })
+      .catch((error) => {
+        console.log("getBlogItem error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
   }
 
   render() {
