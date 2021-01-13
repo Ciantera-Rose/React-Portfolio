78b3765248b6523abd2d71269fd9960ab841b3a5
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-13 13:28:15 -0500

    add call single blog item from API

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class BlogDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {},
+    };
+  }
+
+  getBlogItem() {
+    axios
+      .get(
+        `https://cianterarose.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+      )
+      .then((response) => {
+        this.setState({
+          blogItem: response.data.portfolio_blog,
+        });
+      })
+      .catch((error) => {
+        console.log("getBlogItem error", error);
+      });
+  }
+
   render() {
+    console.log("currentId", this.state.currentId);
     return (
       <div>
         <h1>Blog detail</h1>
