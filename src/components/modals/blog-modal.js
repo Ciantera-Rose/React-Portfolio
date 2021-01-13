e9ea5aba9ef9e6dce1bfab594ca7ee5472a443cc
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-13 18:55:47 -0500

    add modal custom styles

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -1,14 +1,30 @@
+import { faBoxTissue } from "@fortawesome/free-solid-svg-icons";
 import React, { Component } from "react";
 import ReactModal from "react-modal";
 
 export default class BlogModal extends Component {
   constructor(props) {
     super(props);
+
+    this.customStyles = {
+      content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+        width: "800px",
+      },
+      overlay: {
+        backgroundColor: "rgba(1, 1, 1, 0.75)",
+      },
+    };
   }
 
   render() {
     return (
       <ReactModal
+        style={this.customStyles}
         onRequestClose={() => {
           //console.log("testing modal close");
           this.props.handleModalClose();
