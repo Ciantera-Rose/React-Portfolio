67a40a824f855052dc95e7255e3aa77b7f4c588e
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-15 16:08:48 -0500

    add fix state on unmounted component warning

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -43,13 +43,14 @@ export default class BlogForm extends Component {
         { withCredentials: true }
       )
       .then((response) => {
-        this.props.handleSuccessfullFormSubmission(
-          response.data.portfolio_blog
-        );
         this.setState({
           title: "",
           blog_status: "",
+          content: "",
         });
+        this.props.handleSuccessfullFormSubmission(
+          response.data.portfolio_blog
+        );
       })
 
       .catch((error) => {
